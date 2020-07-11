08a6bb0a8a7e630296de4d1c056cfaedb170a822
Harshit Agrawal <42391556+iamharshit13@users.noreply.github.com>
2020-07-11 19:22:16 +0530

    Add files via upload

---
+++ 11_FaceandSmileDetection/1.py
@@ -0,0 +1,27 @@
+import cv2 , time
+
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+smile_cascade = cv2.CascadeClassifier("smile.xml")
+
+video = cv2.VideoCapture(0)
+address = "http://192.168.29.206:8080/video"
+video.open(address)
+while True:
+    check,frame=video.read()
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    face = face_cascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5)
+    for x,y,w,h in face:
+        img = cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),3)
+        smile = smile_cascade.detectMultiScale(gray, scaleFactor=1.9, minNeighbors=20)
+        for x, y, w, h in smile:
+            img = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+    cv2.imshow('Smile',frame)
+    key = cv2.waitKey(1)
+
+    if key == ord('q'):
+        break
+
+
+video.release()
+cv2.destroyAllWindows()
