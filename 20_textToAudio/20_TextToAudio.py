3c8b8ae6cfc3627831f5b1439e8e1bcfde8d5c2f
Harshit Agrawal <42391556+iamharshit13@users.noreply.github.com>
2020-07-20 17:26:40 +0530

    Update and rename audio.py to 20_TextToAudio.py

--- 20_textToAudio/20_TextToAudio.py
+++ 20_textToAudio/20_TextToAudio.py
@@ -1,3 +1,5 @@
+
+#This will narrate the whole text in the rpogram execution itself
 '''import pyttsx3
 def speak(text):
     engine = pyttsx3.init()
@@ -11,6 +13,8 @@ x = f.read().lower()
 speak(x)
 '''
 
+
+#This will read the text and then save it to an audio file in the format mentioned like mp3/wav etc
 from gtts import gTTS
 import os
 f = open('book.txt')
