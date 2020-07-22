951bed320625dec3bea74dd7769c8c5ad69c748b
Harshit Agrawal <42391556+iamharshit13@users.noreply.github.com>
2020-07-22 19:42:05 +0530

    Update and rename yt.py to 22_YouTubeDownloader.py

--- 22_YouTubeDownloader/22_YouTubeDownloader.py
+++ 22_YouTubeDownloader/22_YouTubeDownloader.py
@@ -1,15 +1,19 @@
 from pytube import YouTube
 
 url = input("Enter the link for downloading the video: ")
+
 yt = YouTube(url)
 video = yt.streams
-i=1
 
+i=1
 for quality in video:
     print(str(i) +" "+ str(quality))
     i+=1
 
 selected_qual = int(input("Enter the Number: "))
 file = video[selected_qual-1]
+
+#file will be downloaded in the same directory where the program is.
+#to download in a specific location write the directory inside the download function as : file.download("c:\abc\def")
 file.download()
 print("File Downloaded")
