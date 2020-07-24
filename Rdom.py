f0a412557ada0b10437f34b076305a3d8b9abb6e
Harshit Agrawal <42391556+iamharshit13@users.noreply.github.com>
2020-07-24 22:22:13 +0530

    Add files via upload

---
+++ Rdom.py
@@ -0,0 +1,21 @@
+#AMAZON GIFT CARD GENERATOR
+import random
+import string
+
+def get_random_alphanumeric_string(letters_count, digits_count):
+    sample_str = ''.join((random.choice(string.ascii_letters) for i in range(letters_count))).upper()
+    sample_str += ''.join((random.choice(string.digits) for i in range(digits_count)))
+
+    # Convert string to list and shuffle it to mix letters and digits
+    sample_list = list(sample_str)
+    random.shuffle(sample_list)
+    final_string = ''.join(sample_list)
+    return final_string
+
+a=1
+number= int(input("How many coupon do you want to generate: "))
+for i in range(number):
+    tuning= random.randint(3,7)
+    print(f"Coupon Code {a}:", get_random_alphanumeric_string(15-tuning, tuning))
+    a+=1
+
