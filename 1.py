2a840c6d4b24fcdbc6af794b061ee37105644fbc
Harshit Agrawal <42391556+iamharshit13@users.noreply.github.com>
2020-07-30 20:09:25 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+URL = 'https://www.sms4india.com/api/v1/sendCampaign'
+
+# get request
+def sendPostRequest(reqUrl, apiKey, secretKey, useType, phoneNo, senderId, textMessage):
+  req_params = {
+  'apikey':apiKey,
+  'secret':secretKey,
+  'usetype':useType,
+  'phone': phoneNo,
+  'message':textMessage,
+  'senderid':senderId
+  }
+  return requests.post(reqUrl, req_params)
+
+# get response
+response = sendPostRequest(URL, 'provided-api-key', 'provided-secret', 'prod/stage', 'valid-to-mobile', 'active-sender-id', 'message-text' )
+"""
+  Note:-
+    you must provide apikey, secretkey, usetype, mobile, senderid and message values
+    and then requst to api
+"""
+# print response if you want
+print(response.text)
